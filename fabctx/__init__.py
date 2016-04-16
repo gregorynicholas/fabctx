85b9205dd83122665cd208bee209f0fa68e7ef67
gregory nicholas <nicholas.g.gregory@gmail.com>
2016-04-15 17:46:29 -0700

    - udpates fabctx/__init__.py version, doc strings

--- fabctx/__init__.py
+++ fabctx/__init__.py
@@ -1,14 +1,17 @@
+# encoding: utf-8
 """
   fabctx
   ~~~~~~
 
-
-  http://gregorynicholas.github.io/fabctx
-  :copyright: (c) 2016 by gregorynicholas.
+  :copyright: (c) gregorynicholas.
 """
 from __future__ import unicode_literals
 from pkgutil import extend_path
 __path__ = extend_path(__path__, 'fabctx')
 
 
-__version__ = '0.0.3'
+__version__ = '0.1.0'
+__release__ = 'rc'
+__author__  = 'gregory nicholas'
+__author_email__ = 'gn@gregorynicholas.com'
+__homepage__ = 'http://gregorynicholas.github.io/fabctx'
